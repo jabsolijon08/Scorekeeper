3478463aee9c336e5b24cbfde0b4a51cf4ff59f3
JAMESSOLIJON\James Aljun Solijon <jimbskielim@gmail.com>
2017-07-18 18:39:00 +0800

    Modified my static fragments to dynamic fragments(Steps 2 and 3)

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -1,22 +1,29 @@
 package tech.hyperdev.scorekeeper.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
 import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Fragment mFragment1, mFragment2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ScoreFragment scoreFragmentTeam1 = (ScoreFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentTeam1);
-        ((TextView)scoreFragmentTeam1.getView().findViewById(R.id.tvTeamName)).setText(R.string.team1);
-        ScoreFragment scoreFragmentTeam2 = (ScoreFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentTeam2);
-        ((TextView)scoreFragmentTeam2.getView().findViewById(R.id.tvTeamName)).setText(R.string.team2);
+        mFragment1 = ScoreFragment.newInstance("Team 1");
+        mFragment2 = ScoreFragment.newInstance("Team 2");
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragmentTeam1,mFragment1).replace(R.id.fragmentTeam2,mFragment2).commit();
+//        ScoreFragment scoreFragmentTeam1 = (ScoreFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentTeam1);
+//        ((TextView)scoreFragmentTeam1.getView().findyViewById(R.id.tvTeamName)).setText(R.string.team1);
+//        ScoreFragment scoreFragmentTeam2 = (ScoreFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentTeam2);
+//        ((TextView)scoreFragmentTeam2.getView().findViewById(R.id.tvTeamName)).setText(R.string.team2);
     }
+
 }
--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
 
@@ -13,15 +14,34 @@ import tech.hyperdev.scorekeeper.R;
  */
 public class ScoreFragment extends Fragment {
 
+    private TextView mtvTeamName;
+    private String teamName;
     public ScoreFragment() {
         // Required empty public constructor
+    }
+    public static ScoreFragment newInstance(String someString){
+        ScoreFragment scoreFragment = new ScoreFragment();
+        Bundle args = new Bundle();
+        args.putString("someString",someString);
+        scoreFragment.setArguments(args);
+
+        return scoreFragment;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_score, container, false);
+        mtvTeamName = (TextView)view.findViewById(R.id.tvTeamName);
+        getBundle(getArguments());
 
+        return view;
+    }
+    public void getBundle(Bundle bundle){
+        if(bundle!=null){
+            teamName = bundle.getString("someString");
+            mtvTeamName.setText((teamName));
+        }
+    }
 }
