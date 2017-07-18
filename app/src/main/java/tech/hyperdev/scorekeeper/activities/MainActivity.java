d4371b13d4a7e4af8e5321801f58c375f80679fd
JAMESSOLIJON\James Aljun Solijon <jimbskielim@gmail.com>
2017-07-18 17:28:41 +0800

    Modify team names of each score fragment.

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -1,9 +1,11 @@
 package tech.hyperdev.scorekeeper.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
+import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ScoreFragment scoreFragmentTeam1 = (ScoreFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentTeam1);
+        ((TextView)scoreFragmentTeam1.getView().findViewById(R.id.tvTeamName)).setText(R.string.team1);
+        ScoreFragment scoreFragmentTeam2 = (ScoreFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentTeam2);
+        ((TextView)scoreFragmentTeam2.getView().findViewById(R.id.tvTeamName)).setText(R.string.team2);
     }
 }
